a30a4d7bae42df86f743c1565f5a5f3d86d4760b
test <qacloudqa@gmail.com>
2022-03-24 22:58:26 +0530

    Latest

--- Security/___PDW_PolyBaseAuthorizeSigningCertificate__.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE CERTIFICATE [_##PDW_PolyBaseAuthorizeSigningCertificate##]
-    AUTHORIZATION [dbo]
-    WITH SUBJECT = N'For signing sp_pdw_polybase_authorize SP', START_DATE = N'2022-03-16T09:33:09', EXPIRY_DATE = N'2023-03-16T09:33:09';
-
