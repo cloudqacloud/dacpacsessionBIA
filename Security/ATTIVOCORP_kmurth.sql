5b9b81e64e7ada9bddfeff6069a6a66428431086
test <qacloudqa@gmail.com>
2022-03-24 22:50:25 +0530

    Required Fiels

--- Security/ATTIVOCORP_kmurth.sql
+++
@@ -1,3 +0,0 @@
-﻿CREATE LOGIN [ATTIVOCORP\kmurth]
-    FROM WINDOWS WITH DEFAULT_LANGUAGE = [us_english];
-
--- Security/sa.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE LOGIN [sa]
-    WITH PASSWORD = N'{xp:4i>nRnAyeenj{bfcqCocmsFT7_&#$!~<skhxowvjioon', DEFAULT_LANGUAGE = [us_english];
-
-
-GO
-ALTER LOGIN [sa] DISABLE;
-
